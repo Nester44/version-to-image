c30b3c3902397c2a19e3553f1df449149d5ef55d
Nesterov, Nikita (K-DAPP/4) <nikita.nesterov@volkswagen.de>
2023-12-01 16:27:46 +0100

    refactoring

---
+++ src/utils/fetchAndExtractVersion.ts
@@ -0,0 +1,34 @@
+import { env } from '../env';
+import { ApplicationSide, Stage } from '../types';
+import { extractVersion } from './extractVersion';
+import { getUrl } from './getUrl';
+
+export const fetchAndExtractVersion = async (
+  appName: string,
+  side: ApplicationSide,
+  stage: Stage,
+) => {
+  try {
+    const url = getUrl(env.BITBUCKET_REPOS_URL, appName, side, stage);
+
+    const response = await fetch(url, {
+      headers: { Authorization: `Bearer ${env.BITBUCKET_API_TOKEN}` },
+    });
+
+    if (!response.ok) {
+      throw new Error('Response is not ok');
+    }
+
+    const rawFile = await response.text();
+
+    if (!rawFile) {
+      throw new Error('Raw file is empty');
+    }
+
+    const version = extractVersion(rawFile, side);
+
+    return version;
+  } catch (error: any) {
+    throw new Error(`Error while fetching version: ${error.message}`);
+  }
+};
